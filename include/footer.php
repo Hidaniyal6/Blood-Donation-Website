4f9fcb7f9edbb4e0e40897644ad7929ee8bd6a5b
Muhammad Daniyal Sajid <80337046+Hidaniyal6@users.noreply.github.com>
2024-02-29 18:08:11 +0500

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,87 @@
+
+<footer style="background-color: #e2e0ea;" id="footer my-5">
+    <section class="footer-top">
+      <section class="container-fluid">
+        <section class="row mt-5">
+
+          <section class="col-lg-3 col-md-6 footer-contact mt-4">
+            <h3 style="color:black;text-align:left;" >LIFESAVERBLOOD</h3>
+            <p>
+              M.A Jinnah Road <br>
+              Okara, PC 56300<br>
+              Pakistan <br><br>
+              <strong>Phone:</strong> +92 3157944811<br>
+              <strong>Email:</strong> lifesaverblood@yahoo.com<br>
+            </p>
+          </section>
+
+          <section class="col-lg-2 col-md-6 footer-links mt-4">
+            <h4>Our Links</h4>
+            <ul class="list-unstyled">
+              <li><i class="bx bx-chevron-right"></i><a href="index.php">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="donor.php">Donors</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="search.php">Search</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="signin.php">Sign In</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="about.php">About Us</a></li>
+            </ul>
+          </section>
+
+          <section class="col-lg-3 col-md-6 footer-links mt-4">
+            <h4>Our Goal</h4>
+            <ul class="list-unstyled">
+              <li>Encouraging Donations</li>
+              <li>Promoting Health</li>
+              <li>Simplifying The Process</li>
+              <li>Rising Awareness</li>
+              <li>Emergency Response</li>
+            </ul>
+          </section>
+
+          <section class="col-lg-4 col-md-6 footer-newsletter mt-5">
+            <h4>Join To save life</h4>
+            <p>"Connecting Hearts, Saving Lives: Our Blood Donating Website - Where Compassion Flows and Heroes Unite." 💖🩸</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" class="btn btn-primary" value="subscribe">
+            </form>
+          </section>
+
+        </section>
+      </section>
+    </section>
+
+    <section class="container-fluid d-md-flex py-4 bg-light">
+
+      <section class="me-md-auto text-center text-md-start">
+        <section class="copyright">
+          &copy; Copyright <strong><span>LIFESAVERBLOOD</span></strong>. All Rights Reserved
+        </section>
+        <section class="credits">
+          
+          Designed by Three developers
+        </section>
+      </section>
+      <section class="social-links text-center text-md-right pt-3 pt-md-0 ml-auto mt-4">
+        <a href="#" class="twitter"><i class="fab fa-twitter fa-lg"></i></a>
+        <a href="#" class="facebook"><i class="fab fa-facebook fa-lg"></i></a>
+        <a href="#" class="instagram"><i class="fab fa-instagram fa-lg"></i></a>
+        <a href="#" class="google-plus"><i class="fab fa-skype fa-lg"></i></a>
+        <a href="#" class="linkedin"><i class="fab fa-linkedin fa-lg"></i></a>
+      </section>
+    </section>
+  </footer>
+
+
+
+
+			
+	    <!-- JQuery File -->
+    <script type="text/javascript" src="jquery/jquery.js"></script>
+    
+    <!-- BootStrap JS File-->
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+    
+    <!-- Fontawesome Icon JS-->
+    <script defer src="fontawesome/js/all.js"></script>
+
+	</body>
+</html>
